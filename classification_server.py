cd712c0a43d1180df6c055ce10b80758d608f451
JinQi <aumgli@qq.com>
2019-03-27 20:52:34 +0800

    -增加了多线程 -blahblah

--- classification_server.py
+++ classification_server.py
@@ -1,28 +1,47 @@
 import socket
 import os
 import time
+import threading
+import struct
 from image_classification import do_classification
 
-socket = socket.socket()
+s = socket.socket()
 print(socket)
 IP_PORT = ('127.0.0.1', 5467)
-socket.bind(IP_PORT)
-socket.listen(3)
-print('waiting...')
+s.bind(IP_PORT)
+s.listen(1)
+
+
+def conn_thread(conn):
+    try:
+        conn.settimeout(600)
+        file_info_size = struct.calcsize('l')
+        data = conn.recv(file_info_size)  # 第一次接收待接收文件信息
+        if data or True:
+            file_size = struct.unpack('l', data)[0]
+            print('server file size:', file_size)
+            file_name = str(time.time()).replace('.', '')
+            file_path = os.path.join('image', file_name)
+
+            file = open(file_path, 'ab')
+            received_size = 0
+            while received_size != file_size:  # 第二次接收，开始接收文件数据
+                data = conn.recv(1024)
+                file.write(data)
+                received_size += len(data)
+            file.close()
+            # conn.close()
+            result_label = do_classification(file_path)
+            resp = struct.pack('l', result_label)
+            conn.sendall(resp)
+            print('image is belong to label', result_label)
+    except socket.error as e:
+        print(e)
+
 
 while True:
-    conn, addr = socket.accept()  # 第一次接收待接收文件信息
-    data = conn.recv(1024)
-    file_size = int(str(data, 'utf8'))
-    file_name = str(time.time()).replace('.', '')
-    file_path = os.path.join('image', file_name)
-
-    file = open(file_path, 'ab')
-    received_size = 0
-    while received_size != file_size:  # 第二次接收，开始接收文件数据
-        data = conn.recv(1024)
-        file.write(data)
-        received_size += len(data)
-    file.close()
-    print('image is belong to label', do_classification(file_path))
     print('waiting...')
+    conn, addr = s.accept()
+    print('connected by:', addr)
+    thread = threading.Thread(target=conn_thread, args=(conn,))
+    thread.start()
--- test_client.py
+++ test_client.py
@@ -1,28 +1,34 @@
 import socket
 import os
+import struct
 
-socket = socket.socket()
-print(socket)
-address = ('127.0.0.1', 5467)
-socket.connect(address)
-# BASE_DIR
+s = socket.socket()
+print(s)
+REMOTE_IP_PORT = ('127.0.0.1', 5467)
+LOCAL_IP_PORT = ('127.0.0.1', 5468)
+# s.bind(LOCAL_IP_PORT)
+s.connect(REMOTE_IP_PORT)
 
 while True:
     req_method = 'post'
-    image = 'test_image/35995.jpg'
+    image = 'test_image/1483351.jpg'
     file_name = os.path.basename(image)
     file_size = os.stat(image).st_size
-    # print('file name:', file_name, ', file size:', file_size)
-    # file_info = req_method + '|' + file_name + '|' + str(file_size)
-    # print(file_info)
-    socket.sendall(bytes(str(file_size), 'utf8'))
+    file_info = struct.pack('l', file_size)
+    s.sendall(file_info)
+    # print('client file size:', file_size)
+    # s.sendall(bytes(str(file_size), 'utf8'))
 
     file = open(image, 'rb')
     sent_size = 0
     while sent_size != file_size:
         data = file.read(1024)
-        socket.sendall(data)
+        s.sendall(data)
         sent_size += len(data)
     file.close()
     print('Upload successfully.')
+    resp_size = struct.calcsize('l')
+    label_result = s.recv(resp_size)
+    label_result = struct.unpack('l', label_result)[0]
+    print('response label:', label_result)
     break
