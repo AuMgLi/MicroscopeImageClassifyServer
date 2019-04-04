7ba3d5b9f5be3a84d77d52e325ea37c4b3138fb0
JinQi <aumgli@qq.com>
2019-04-04 17:57:01 +0800

    -写了个monitor线程来监控socket线程，当某个socket线程结束时删除其生成的图片文件；

--- classification_server.py
+++ classification_server.py
@@ -4,7 +4,11 @@ import time
 import threading
 import struct
 import hashlib
+import queue
 from image_classification import do_classification, ERR_CANNOT_IDENTIFY_IMAGE
+
+q = queue.Queue()
+socket_threads_list = []
 
 ERR_TIMEOUT_WHILE_RECV_DATA = -2
 ERR_MD5_INCONSISTENT = -3
@@ -19,12 +23,13 @@ s.listen(1)
 
 
 def conn_thread(connection):
+
     try:
         connection.settimeout(40)
         file_info_size = struct.calcsize('l32s')
         # print('[server]', file_info_size)
         data = connection.recv(file_info_size)  # 第一次接收待接收文件的大小及MD5信息
-        print('[server] data length:', len(data))
+        # print('[server] data length:', len(data))
         try:
             file_size, md5 = struct.unpack('l32s', data)
             md5 = str(md5, encoding='utf8')
@@ -46,6 +51,8 @@ def conn_thread(connection):
         file_name = str(time.time()).replace('.', '')
         file_path = os.path.join('image', file_name)
         file = open(file_path, 'ab')
+
+        _put_thread_id_and_image_path_into_queue(threading.current_thread().ident, file_path)
         connection.settimeout(10)
         try:
             received_size = 0
@@ -53,6 +60,8 @@ def conn_thread(connection):
                 data = connection.recv(1024)
                 file.write(data)
                 received_size += len(data)
+                if len(data) == 0:
+                    raise Exception('Server got nothing while receiving file data.')
             print('[server] File', file_path, 'has been successfully downloaded.')
         except socket.timeout as e:
             print(e)
@@ -64,7 +73,7 @@ def conn_thread(connection):
             # print('[server] response sent')
             connection.close()
             file.close()
-            os.remove(file_path)
+            # os.remove(file_path)
             return
         except Exception as e:
             print(e)
@@ -74,7 +83,7 @@ def conn_thread(connection):
             connection.sendall(resp)
             connection.close()
             file.close()
-            os.remove(file_path)
+            # os.remove(file_path)
             return
         finally:
             file.close()
@@ -110,7 +119,7 @@ def conn_thread(connection):
         connection.sendall(resp)
         connection.close()
         print('result label is', result_label)
-        os.remove(file_path)
+        # os.remove(file_path)
     except socket.timeout as e1:
         print(e1)
         server_msg = 'server_response_code: [' + str(ERR_TIMEOUT_WHILE_RECV_INFO) + \
@@ -122,8 +131,42 @@ def conn_thread(connection):
         print(e2)
 
 
+def _put_thread_id_and_image_path_into_queue(thread_id, image_path):
+    id_path_dict = dict([(thread_id, image_path)])
+    q.put(id_path_dict)
+
+
+def monitor_thread(socket_threads):
+    """
+    在进程结束后删除缓存的图片文件
+    :param socket_threads:
+    """
+    thread_id_image_path_dict = None
+    while True:
+        if not q.empty():
+            id_and_path = q.get()
+            if thread_id_image_path_dict is None:
+                thread_id_image_path_dict = id_and_path
+            else:
+                thread_id_image_path_dict.update(id_and_path)
+            # print('id & path:', thread_id_image_path_dict)
+        for socket_t in socket_threads:
+            if not socket_t.is_alive():
+                socket_id = socket_t.ident
+                # print('dead thread id:', socket_id)
+                socket_threads.remove(socket_t)
+                path_to_delete = thread_id_image_path_dict.pop(socket_id)
+                # print('path_to_delete:', path_to_delete, ', dict:', thread_id_image_path_dict)
+                if os.path.exists(path_to_delete):
+                    os.remove(path_to_delete)
+                print('[server] ' + path_to_delete + ' deleted by monitor.')
+
+
+threading.Thread(target=monitor_thread, args=(socket_threads_list,)).start()
+
 while True:
     conn, addr = s.accept()
     print('connected by:', addr)
     thread = threading.Thread(target=conn_thread, args=(conn,))
     thread.start()
+    socket_threads_list.append(thread)
